e2a247d0566a42fca53f210dc8bfb4cdb9215e5e
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-05-22 12:21:42 +0200

    create new mapper method to map from City entity to desired output format

--- src/main/java/com/forflow/springinterview/mapper/CityOutboundDTOMapper.java
+++ src/main/java/com/forflow/springinterview/mapper/CityOutboundDTOMapper.java
@@ -2,6 +2,7 @@ package com.forflow.springinterview.mapper;
 
 import com.forflow.springinterview.dto.CityOutboundDTO;
 import com.forflow.springinterview.dto.GeoDBResponse;
+import com.forflow.springinterview.entity.City;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -16,6 +17,16 @@ public class CityOutboundDTOMapper {
         city.setLongitude(data.getLongitude());
         city.setWikiDataId(data.getWikiDataId());
         city.setCountryIsoCode(data.getCountryCode());
+        return city;
+    }
+    public static CityOutboundDTO createFrom(City cityEntity) {
+        if (cityEntity == null) return null;
+        CityOutboundDTO city = new CityOutboundDTO();
+        city.setName(cityEntity.getName());
+        city.setLatitude(cityEntity.getLatitude());
+        city.setLongitude(cityEntity.getLongitude());
+        city.setWikiDataId(cityEntity.getWikiDataId());
+        city.setCountryIsoCode(cityEntity.getCountryIsoCode());
         return city;
     }
 
